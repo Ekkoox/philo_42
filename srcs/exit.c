f4f29b099bdd1ea3ff70250a0a514aa349709813
dsatge <deborah.satge@yahoo.fr>
2025-04-24 16:30:44 +0200

    check valid arguments convert_nbr

--- srcs/exit.c
+++ srcs/exit.c
@@ -1,20 +1,20 @@
 # include "philo.h"
 
-void	destroy_mutex(t_philo *phill)
-{
-	if (&phill->routine->stop_mutex == 1)
-		pthread_mutex_destroy(&phill->routine->stop_mutex);
-	if (&phill->routine->meals_mutex == 2)
-		pthread_mutex_destroy(&phill->routine->meals_mutex);
-	if (&phill->data->left_forks == 3)
-		pthread_mutex_destroy(&phill->data->left_forks);
-	if (&phill->data->right_forks == 4)
-		pthread_mutex_destroy(&phill->data->right_forks);
-}
+// void	destroy_mutex(t_philo *phill)
+// {
+// 	if (&phill->routine->stop_mutex == 1)
+// 		pthread_mutex_destroy(&phill->routine->stop_mutex);
+// 	if (&phill->routine->meals_mutex == 2)
+// 		pthread_mutex_destroy(&phill->routine->meals_mutex);
+// 	if (&phill->data->left_forks == 3)
+// 		pthread_mutex_destroy(&phill->data->left_forks);
+// 	if (&phill->data->right_forks == 4)
+// 		pthread_mutex_destroy(&phill->data->right_forks);
+// }
 
-int	check_philo(t_philo *phill)
-{
-	pthread_mutex_lock(&phill)
-}
+// int	check_philo(t_philo *phill)
+// {
+// 	pthread_mutex_lock(&phill);
+// }
 //check si manager a um philo mort, si oui exit les autres
 // 
